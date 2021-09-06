5f8f724e2c24b20fb45bafc2efce04334bb7b302
VenomDV <77664941+VenomDV@users.noreply.github.com>
2021-09-06 10:18:33 +0200

    Add files via upload

---
+++ While_for.py
@@ -0,0 +1,10 @@
+"""nbmeleve = ""
+x = 0
+while nbmeleve != "q" and x <= 11:
+    nbmeleve = input("Quelle est le nom de l'élève présent ? : ")
+    x +=1
+"""
+
+for nbmeleve in range (11):
+    input("Quelle est le nom de l'élève présent ? : ")
+
