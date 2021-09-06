9e65d49f32a26c5d97256b9c6d1f63980433ed31
VenomDV <77664941+VenomDV@users.noreply.github.com>
2021-09-06 08:53:34 +0200

    Add files via upload

---
+++ Venuyan.py
@@ -0,0 +1,6 @@
+nom=input("comment vous appelez vous ? : ")
+age=int(input("vous avez quelle age ? : "))
+print(f"Hello votre nom est {nom} et vous avez {age} ans.")
+print("Hello votre nom est" +nom+ "et vous avez" ,age, "ans.")
+print("Hello votre nom est {} et vous avez {} ans.".format(nom,age))
+print("Hello votre nom est" +nom+ "et vous avez " +str(age)+ " ans.")
